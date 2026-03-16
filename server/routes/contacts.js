92edda6f84bd9683cd201dd980302deff34fa4a3
Siyi Ji <sylviajsyy@gmail.com>
2026-03-16 16:34:10 -0500

    GET one contact

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -30,4 +30,34 @@ router.get('/', async (req,res) => {
     }
 })
 
+// Get one contact
+router.get("/:id", async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+        const result = await db.query(`
+            SELECT
+                contacts.id,
+                contacts.name,
+                contacts.email,
+                contacts.phone,
+                contacts.notes,
+                COALESCE(STRING_AGG(groups.name, ', '), 'No Group') AS groups
+            FROM contacts
+            LEFT JOIN contact_groups
+                ON contacts.id = contact_groups.contact_id
+            LEFT JOIN groups
+                ON groups.id = contact_groups.group_id
+            WHERE contacts.id=$1
+            GROUP BY contacts.id
+        `,[id]);
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to fetch contacts" });
+    }
+})
+
 export default router;
