8eeb933ec74e27f107ec2c798d7ed75ac82a8b3e
Siyi Ji <sylviajsyy@gmail.com>
2026-03-23 21:40:24 -0500

    Refactor GET Groups logic backend

--- server/routes/group.js
+++ server/routes/group.js
@@ -6,45 +6,41 @@ const router = express.Router();
 // Get all groups
 router.get('/', async (req,res) => {
     try {
-        const result = await db.query(
+        const groups = await db.query(
             `SELECT id, name
             FROM groups
             ORDER BY name`
         );
 
-        res.json(result.rows);
+        let result = [];
+
+        for (const group of groups.rows){
+            const contacts = await db.query(
+                `
+                SELECT
+                    contacts.id,
+                    contacts.name,
+                    contacts.email,
+                    contacts.phone,
+                    contacts.notes
+                FROM contacts
+                JOIN contact_groups cg ON contacts.id = cg.contact_id
+                WHERE cg.group_id = $1
+                ORDER BY contacts.name;
+                `,
+                [group.id]
+            );
+
+            result.push({
+                ...group,
+                contacts:contacts.rows,
+            })
+        }
+
+        res.json(result);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Failed to fetch groups" });
-    }
-})
-
-// Get all contacts under a group
-router.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const result = await db.query(
-            `
-            SELECT
-                contacts.id,
-                contacts.name,
-                contacts.email,
-                contacts.phone,
-                contacts.notes
-            FROM contacts
-            JOIN contact_groups cg ON contacts.id = cg.contact_id
-            WHERE cg.group_id = $1
-            GROUP BY contacts.id
-            ORDER BY contacts.name;
-            `,
-            [id]
-        );
-
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Failed to fetch group contacts" });
     }
 })
 
