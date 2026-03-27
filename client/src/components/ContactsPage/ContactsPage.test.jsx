f529f89d537dc3746bf9a1cdff4fab6afba0ed15
Siyi Ji <sylviajsyy@gmail.com>
2026-03-26 22:17:29 -0500

    opens edit modal with prefilled contact data Integration test

--- client/src/components/ContactsPage/ContactsPage.test.jsx
+++ client/src/components/ContactsPage/ContactsPage.test.jsx
@@ -47,4 +47,48 @@ describe('Contacts Page Integration Test', () => {
         expect(await screen.findByText("Alice Johnson")).toBeInTheDocument();
         expect(await screen.findByText("Bob Smith")).toBeInTheDocument();
     })
+    
+    test('opens edit modal with prefilled contact data', async () => {
+        const user = userEvent.setup();
+
+        vi.spyOn(global, "fetch")
+        // first call: loadContacts
+        .mockResolvedValueOnce({
+            ok: true,
+            json: async () => mockContacts,
+        })
+        // second call: handleEdit -> fetch one contact
+        .mockResolvedValueOnce({
+            ok: true,
+            json: async () => ({
+                id: 1,
+                name: "Alice Johnson",
+                email: "alice@example.com",
+                phone: "512-123-4567",
+                notes: "Friend",
+                groups: "Family",
+                group_id: [1,2],
+            }),
+        })
+        // third call: ContactForm loadGroups
+        .mockResolvedValueOnce({
+            ok: true,
+            json: async () => [
+                { id: 1, name: "Family" },
+                { id: 2, name: "Work" },
+            ],
+        })
+
+        render(<ContactsPage />);
+
+        expect(await screen.findByText("Alice Johnson")).toBeInTheDocument();
+
+        const editButtons = screen.getAllByTestId("edit-contact-btn");
+        await user.click(editButtons[0]);
+
+        await waitFor(() => {
+            expect(screen.getByDisplayValue("Alice Johnson")).toBeInTheDocument();
+            expect(screen.getByDisplayValue("alice@example.com")).toBeInTheDocument();
+        })
+    })
 })
